4e5d956cd982778d8618286095591cb0f9cd3c3d
elgun <elgun1999@gmail.com>
2018-09-03 12:35:31 +0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,11 +28,13 @@ socket.on('newLocationMessage', function(message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
+  var messageTextbox=jQuery('[name=message]');
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function() {
-
+    messageTextbox.val('');
   });
 });
 
@@ -41,12 +43,15 @@ locationButton.on('click', function() {
   if (!navigator.geolocation) {
     return alert('Gerolocation not supported');
   }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
   navigator.geolocation.getCurrentPosition(function(position) {
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage', {
       lat: position.coords.latitude,
       lon: position.coords.longitude
     })
   }, function () {
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   })
 });
